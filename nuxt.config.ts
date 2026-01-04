71a7523c5a6969ccd334d7af47d7c73cb8e0f1c8
dashaBobrovad <dbobrova.dasha@gmail.com>
2026-01-04 12:12:14 +0300

    chore: switch to pnpm and update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,4 @@
-import { defineNuxtConfig } from 'nuxt/config';
-
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+	compatibilityDate: '2026-01-04',
 	modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt'],
-});
+})
